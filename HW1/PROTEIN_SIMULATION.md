b80de8f9570ac0b4d1c4ae5594d75820ed272134
Peter Rosenthal <peter@rosenth.al>
2021-02-08 10:25:59 -0700

    Add protein simulation results

--- HW1/PROTEIN_SIMULATION.md
+++ HW1/PROTEIN_SIMULATION.md
@@ -1,1 +1,58 @@
-Write-up for HW1 part 1 will go here.
+# Coarse Grained Simulation of a Protein
+Part one of this homework assignment is to run a simple coarse grained protein folding simulation.
+## (1) Investigating varying N
+For part one of part one of this homework, we are to prepare a plot of the radius of gyration showcasing simulations run for varying `N`, the # of monomers in the chain, or just the length of the chain. The first step is to write a function to calculate the radius of gyration.
+
+The homework assignment gives an equation for the radius of gyration, but I wasn't feeling certain about how to interpret the squaring of a vector. The way that I first decided to do it was to sum up the squared distances between `r_i` and `r_j` (i.e. the magnitude of the differences), rather than the raw differences between `r_i` and `r_j`. Then later on I remembered that a more proper way to interpret squaring a vector was to take the dot product with itself. Luckily for me, the squared magnitude of a vector (the initial solution I went with), is the same thing as a vector dot product with itself. This inspired me to fix/speed up my code by removing a redundant square root followed by a squaring. This is the function for that version of the radius of gyration:
+```python
+def radius_of_gyration(N, x):
+    sum_rad_gy = 0
+    for i in range(0, N):
+        for j in range(0, N):
+            if not i == j:
+                diff = (x[i, :] - x[j, :])
+                sum_rad_gy += (diff[0] * diff[0] + diff[1] * diff[1])
+    return sum_rad_gy / (2 * N * N)
+```
+I still wasn't feeling 100% trustworthy of that function however. So I decided to also try also learning about the radius of gyration from the internet's most reputable source: Wikipedia. According to Wikipedia, the radius of gyration for a set of points is the root mean square of the distances of each point from the center of mass of all the points. So I wrote a function to first calculate the center of mass, and then calculate the RMS of the distance of each point from that center of mass:
+```python
+def radius_of_gyration_rms(N, x):
+    center_of_mass = np.zeros(2)
+    sum_rad_gy = 0
+    for i in range(0, N):
+        center_of_mass += x[i, :]
+    center_of_mass /= N
+    for i in range(0, N):
+        diff = (center_of_mass - x[i, :])
+        sum_rad_gy += (diff[0] * diff[0] + diff[1] * diff[1])
+    return math.sqrt(sum_rad_gy / N)
+```
+Now with both functions completed, I could compare them by plotting both values as functions of time, for each of the `N = 25, 20, 5`. The version from the HW1 PDF is shown first below, and the Wikipedia definition is shown second. Both are very similar, and show the same overall behavior throughout the simulation: the radius of gyration starts high and decreases throughout the simulation, reaching close to it's minimum by 100,000 steps for `N = 25` and `N = 20`. But the short polymer, with a length of just `N = 5` reaches near its minimum radius of gyration much quicker, around 10,000 steps.
+ 
+The major difference between the two graphs (using the two different radius of gyration definitions), is that the Wikipedia definition is consistently on the scale of about a square root of the HW1 PDF version. This is kind of to be expected however, because aside from a square root, the two different functions are essentially doing the same thing. The only real difference between the two, other than the square root, is that the HW1 version is summing N times more interactions, because it is taking the average distance between two particles in the system, rather than the average distance of each particle from one given point (the center of mass). And this N times more sums is also accounted for in the difference between the two functions: one is divided by an `N^2` at the end while the other is divided by just a `sqrt(N)`.
+
+![Part 1 graph (HW1 PDF Version)](part_1-radius_of_gyration.png)
+![Part 1 graph (Wikipedia Version)](part_1-radius_of_gyration_rms.png)
+
+## (2) Investigating varying LJ potential strengths
+The second part of the first part of the homework is very similar to the first part of the first part, in that we are running a parameter sweep, and investigating the radius of gyration across the parameter sweep. The only thing that has changed since the first part is that we are no longer investigating differences in `N`, the chain length, and instead we are investigating differences in `epsilon_LJ`, the strength of the Lennard-Jones potential. Once again I decided to just use both functions for the radius of gyration simultaneously.
+
+Here, the HW1 PDF version and the Wikipedia version follow the same relationship as discussed before. As for why the versions of the simulation follow the trend of starting high if `epsilon_LJ = 1`, and low otherwise, I'm really not sure. I'm not even sure if this is supposed to be the behavior I should expect. But I don't have enough time to do any more investigating, these simulations can take quite some time, and I got distracted trying to have fun and make gif animations of these simulations.
+
+![Part 2 graph (HW1 PDF Version)](part_2-radius_of_gyration.png)
+![Part 2 graph (Wikipedia Version)](part_2-radius_of_gyration_rms.png)
+
+All of the graphs for these two sections were created with a quick and dirty function that loads the data from a file and uses pyplot to create the graphs and save them as .png's for me. I decided to save the data to a .npy file at the end of simulation, and load it again from the file at the beginning of visualization, because it allowed me to make as many small and incremental changes as I wanted to the visualization functions without requiring me to go back and re-run the simulation(s) over and over again.
+
+```python
+def plot_radius_of_gyration(in_file, out_file, title, legend):
+    radii = np.load(in_file)
+    for radius in radii:
+        plt.plot(np.arange(1, len(radius) + 1), radius)
+    plt.title(title)
+    plt.legend(legend)
+    plt.xlabel("timestep")
+    plt.ylabel("radius of gyration")
+    plt.savefig(out_file, format="png")
+    plt.clf()
+```
--- HW1/polymer_MD_Python.py
+++ HW1/polymer_MD_Python.py
@@ -1,40 +1,10 @@
 import matplotlib.pyplot as plt
 import math
 import numpy as np
-import random
-
-
-#################################################################
-def Polymer_MD_Python():
-    # initialize parameters
-    N = 25  # no. of particles
-    T = 0.05  # temperature
-    dt = 0.0005  # integration time step
-    steps = 500000  # time steps
-
-    epsilon_LJ = 1
-    cutoff_LJ = 2.5
-    spring_coeff = 5
-    min_sep = 1.122
-
-    L = min_sep * N
-    print_interval = 1000
-    ####################################################################
-    # initialize position velocities and forces
-    x, v = initial_configuration(min_sep, N)
-    pairs = []
-    #####################################################################
-    # main loop
-    for step_i in range(0, steps):  # molecular dynamics loop
-        if np.mod(step_i - 1, print_interval) == 0:
-            mytitle = ["step=", str(step_i), "N=", str(N), "L=", str(L)]
-            print(mytitle)
-        x, pairs = steepest_descent(N, x, dt, cutoff_LJ, epsilon_LJ, min_sep, spring_coeff, T)
-    visualize_particles(N, x, L, pairs, mytitle)
 
 
 def Polymer_MD_Vary_N():
-    # Loop over various N and plot the result
+    # Loop over various N and save the result
     N = [25, 20, 5]
     T = 0.05
     dt = 0.0005
@@ -45,23 +15,28 @@ def Polymer_MD_Vary_N():
     spring_coeff = 5
     min_sep = 1.122
 
-    print_interval = 1000
-    for n in N:
-        L = min_sep * n
+    print_interval = 500
 
-        x, v = initial_configuration(min_sep, n)
-        pairs = []
+    radius_gy = np.zeros((len(N), steps))
+    radius_gy_rms = np.zeros((len(N), steps))
+
+    for n_i, n in enumerate(N):
+        x = initial_configuration(min_sep, n)
 
         for step_i in range(0, steps):
+            x, pairs = steepest_descent(n, x, dt, cutoff_LJ, epsilon_LJ, min_sep, spring_coeff, T)
+            radius_gy[n_i, step_i] = radius_of_gyration(n, x)
+            radius_gy_rms[n_i, step_i] = radius_of_gyration_rms(n, x)
             if np.mod(step_i - 1, print_interval) == 0:
-                mytitle = ["step=", str(step_i), "N=", str(n), "L=", str(L)]
-                print(mytitle)
-            x, pairs = steepest_descent(N, x, dt, cutoff_LJ, epsilon_LJ, min_sep, spring_coeff, T)
-        visualize_particles(n, x, L, pairs, mytitle)
+                print("step =", step_i, "  N =", n, "  radius_of_gyration =", radius_gy[n_i, step_i])
+                visualize_particles(x, step_i, print_interval, "part-1-" + str(n_i))
+
+    np.save("part_1-radius_of_gyration.npy", radius_gy)
+    np.save("part_1-radius_of_gyration_rms.npy", radius_gy_rms)
 
 
 def Polymer_MD_Vary_LJ():
-    # Loop over various epsilon_LJ and plot the result
+    # Loop over various epsilon_LJ and save the result
     N = 25
     T = 0.05
     dt = 0.0005
@@ -72,31 +47,34 @@ def Polymer_MD_Vary_LJ():
     spring_coeff = 5
     min_sep = 1.122
 
-    L = min_sep * N
-    print_interval = 1000
+    print_interval = 500
 
-    x, v = initial_configuration(min_sep, N)
-    for e in epsilon_LJ:
-        pairs = []
+    radius_gy = np.zeros((len(epsilon_LJ), steps))
+    radius_gy_rms = np.zeros((len(epsilon_LJ), steps))
+
+    x = initial_configuration(min_sep, N)
+    for e_i, e in enumerate(epsilon_LJ):
 
         for step_i in range(0, steps):
-            if np.mod(step_i - 1, print_interval) == 0:
-                mytitle = ["step=", str(step_i), "N=", str(N), "L=", str(L)]
-                print(mytitle)
             x, pairs = steepest_descent(N, x, dt, cutoff_LJ, e, min_sep, spring_coeff, T)
-        visualize_particles(N, x, L, pairs, mytitle)
+            radius_gy[e_i, step_i] = radius_of_gyration(N, x)
+            radius_gy_rms[e_i, step_i] = radius_of_gyration_rms(N, x)
+            if np.mod(step_i - 1, print_interval) == 0:
+                print("step =", step_i, "  epsilon_LJ =", e, "  radius_of_gyration =", radius_gy[e_i, step_i])
+                visualize_particles(x, step_i, print_interval, "part-2-" + str(e_i))
+
+    np.save("part_2-radius_of_gyration.npy", radius_gy)
+    np.save("part_2-radius_of_gyration_rms.npy", radius_gy_rms)
 
 
 def initial_configuration(initial_min_sep, N):
     x = np.zeros((N, 2))
-    v = np.zeros((N, 2))
     for i in range(0, N):
         x[i][0] = initial_min_sep * i - (initial_min_sep * N / 2)
-    return x, v
+    return x
 
 
 def steepest_descent(N, x, dt, cutoff_LJ, epsilon_LJ, min_sep, spring_coeff, T):
-    # velocity verlet integrator
     F_particles, _, pairs = forces(N, x, cutoff_LJ, epsilon_LJ, min_sep, spring_coeff)
     F = F_particles
     x = x + (dt * F) + np.dot(T, (np.random.rand(x.shape[0], x.shape[1]) - 0.5))
@@ -109,10 +87,10 @@ def all_interactions(N, x, cutoff):  # obtain interacting pairs
     pair = []
     for i in range(0, N - 1):
         for j in range(i + 1, N):
-            distance = x[j, :] - x[i, :]
-            if np.linalg.norm(distance) < cutoff:  # only interacting pairs (cutoff+shell for neighbor lists)
-                ip = ip + 1  # interaction pair counter
-                pair.append([i, j])  # particle numbers (i,j) belonging to pair (ip)
+            distance = x[j, :] - x[i, :]  # distance : (1x2)
+            if np.linalg.norm(distance) < cutoff:
+                ip = ip + 1
+                pair.append([i, j])
                 connector.append([distance])
     return ip, pair, connector
 
@@ -148,44 +126,90 @@ def forces(N, x, cutoff_LJ, epsilon_LJ, min_sep, spring_coeff):
     for i in range(0, no):
         FORCE = force_springs(connector[i], spring_coeff, min_sep)
         F[pair[i][0]] = F[pair[i][0]] - FORCE
-        F[pair[i][1]] = F[pair[i][1]] + FORCE  # actio = reactio;
+        F[pair[i][1]] = F[pair[i][1]] + FORCE  # action = reaction;
         P[pair[i][0]] = P[pair[i][0]] + (np.sum(FORCE * connector[i], axis=0))
         P[pair[i][1]] = P[pair[i][1]] + (np.sum(FORCE * connector[i], axis=0))
     return F, P, pair
 
 
 def force_springs(r_vector, spring_coeff_array, min_sep):
-    r2 = np.asarray(np.square(r_vector) + 2)
+    r2 = np.sum(np.square(r_vector), axis=1)
     r = np.sqrt(r2)
     r = r.flatten()
     curr_force = np.zeros((len(r2), 2))
-    val_1 = np.dot((np.divide(r_vector[0][0], r)), np.subtract(r, min_sep), out=None)
-    val_2 = np.dot(np.subtract(r, min_sep), (np.divide(r_vector[0][1], r)))
-    curr_force[0][0] = np.dot(np.transpose(-spring_coeff_array), val_1)
-    curr_force[0][1] = np.dot(np.transpose(-spring_coeff_array), val_2)
+    val_1 = np.multiply(np.subtract(r, min_sep), np.divide(r_vector[0][0], r), out=None)
+    val_2 = np.multiply(np.subtract(r, min_sep), np.divide(r_vector[0][1], r), out=None)
+    curr_force[0][0] = np.multiply(np.transpose(-spring_coeff_array), val_1)
+    curr_force[0][1] = np.multiply(np.transpose(-spring_coeff_array), val_2)
     return curr_force
 
 
 def force_LJ(r_vector, epsilon_LJ):
-    r = np.linalg.norm(r_vector)  # two body force
+    r = np.linalg.norm(r_vector)
     force_LJ = 24 * epsilon_LJ * np.dot((np.dot(2, r ** (-14)) - r ** (-8)), r_vector)
     return force_LJ
 
 
-def visualize_particles(N, x, L, pairs, mytitle):
-    X = [i[0] for i in x]
-    Y = [i[1] for i in x]
-    colors = (0, 0, 0)
-    plt.ylim(top=10, bottom=-10)
-    plt.xlim([-10, 10])
-    plt.scatter(X, Y, c=colors, alpha=0.5)
-    plt.title(mytitle)
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.show()
-    return
+def radius_of_gyration(N, x):
+    sum_rad_gy = 0
+    for i in range(0, N):
+        for j in range(0, N):
+            if not i == j:
+                diff = (x[i, :] - x[j, :])
+                sum_rad_gy += (diff[0] * diff[0] + diff[1] * diff[1])
+    return sum_rad_gy / (2 * N * N)
 
-# Un-comment whichever method to use
-#Polymer_MD_Python()
-#Polymer_MD_Vary_N()
-#Polymer_MD_Vary_LJ()
+
+def radius_of_gyration_rms(N, x):
+    center_of_mass = np.zeros(2)
+    sum_rad_gy = 0
+    for i in range(0, N):
+        center_of_mass += x[i, :]
+    center_of_mass /= N
+    for i in range(0, N):
+        diff = (center_of_mass - x[i, :])
+        sum_rad_gy += (diff[0] * diff[0] + diff[1] * diff[1])
+    return math.sqrt(sum_rad_gy / N)
+
+
+def plot_radius_of_gyration(in_file, out_file, title, legend):
+    radii = np.load(in_file)
+    for radius in radii:
+        plt.plot(np.arange(1, len(radius) + 1), radius)
+    plt.title(title)
+    plt.legend(legend)
+    plt.xlabel("timestep")
+    plt.ylabel("radius of gyration")
+    plt.savefig(out_file, format="png")
+    plt.clf()
+
+
+def visualize_particles(x, step, save_interval, folder):
+    plt.ylim([-10, 10])
+    plt.xlim([-10, 10])
+    plt.scatter(x[:, 0], x[:, 1])
+    frame_num = (step - 1) / save_interval + 1
+    plt.savefig(folder + "/" + f"{frame_num:06d}" + ".png", format="png")
+    plt.clf()
+
+
+# Step 1: Modified "main" method for calculating part 1
+Polymer_MD_Vary_N()
+
+# Step 2: Modified "main" method for calculating part 2
+Polymer_MD_Vary_LJ()
+
+# Step 3: Create graphs from the data calculated in parts 1 and 2
+plot_radius_of_gyration("part_1-radius_of_gyration.npy", "part_1-radius_of_gyration.png",
+                        "Radius of Gyration\n(HW1 PDF definition)", ["N=25", "N=20", "N=5"])
+
+plot_radius_of_gyration("part_1-radius_of_gyration_rms.npy", "part_1-radius_of_gyration_rms.png",
+                        "Radius of Gyration\n(Wikipedia definition)", ["N=25", "N=20", "N=5"])
+
+plot_radius_of_gyration("part_2-radius_of_gyration.npy", "part_2-radius_of_gyration.png",
+                        "Radius of Gyration\n(HW1 PDF definition)",
+                        ["epsilon_LJ=1", "epsilon_LJ=0.5", "epsilon_LJ=0"])
+
+plot_radius_of_gyration("part_2-radius_of_gyration_rms.npy", "part_2-radius_of_gyration_rms.png",
+                        "Radius of Gyration\n(Wikipedia definition)",
+                        ["epsilon_LJ=1", "epsilon_LJ=0.5", "epsilon_LJ=0"])
